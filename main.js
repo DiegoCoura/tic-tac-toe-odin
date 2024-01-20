6536397d156605c471312c95dec475df33f02acd
DiegoCoura <diegogomes.10.9@gmail.com>
2024-01-19 23:18:28 -0300

    feat: add game functionalities

--- main.js
+++ main.js
@@ -1,66 +1,39 @@
-import { Gameboard } from "./components/Gameboard.js";
+import { game } from "./components/game.js";
 
 const mainContent = document.getElementById("main-content");
 
-const newBoard = Gameboard();
+let gameStatus = false;
 
-mainContent.appendChild(newBoard);
-
-let currentPlayer = "Player 1";
-let moves = [];
-let winner = ""
-
-function placeMove(position) {
-  let currentCell = position;
-
-  if (currentCell.innerText) return;
-
-  if (currentPlayer === "Player 1") {
-    currentCell.innerText = "X";
-    currentPlayer = "Player 2";
-  } else if (currentPlayer === "Player 2") {
-    currentCell.innerText = "O";
-    currentPlayer = "Player 1";
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
-  moves.push(`${currentCell.innerText}: ${currentCell.id}`);
-
-  moves.length >= 5 ? checkWinner() : "";
-
-  if(checkWinner){
-    console.log(winner)
-  }
-  
 }
 
-const cells = document.querySelectorAll(".cell");
-
-for (let cell of cells) {
-  cell.addEventListener("click", () => placeMove(cell));
+function playGame() {
+  gameStatus = true;
+  removeChildNodes(mainContent);
+  const showBoard = game();
 }
 
-function checkWinner() {
-  const combs = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+function showMenu() {
 
-  for(let i = 0; i < combs.length; i++){
-    const [a, b, c] = combs[i];
+  const menu = document.createElement("div");
+  menu.setAttribute("id", "menu");
+  const startBtn = document.createElement("button");
+  startBtn.classList.add("start-button");
+  startBtn.innerText = "Start";
+  startBtn.addEventListener("click", () => playGame());
 
-    if(cells[a].innerText && cells[a].innerText === cells[b].innerText && cells[a].innerText === cells[c].innerText){
-        if(cells[a].innerText === "X"){
-            winner = "Player 1"
-        } else{
-            winner = "Player 2"
-        }
-        return winner;
-    }
-  }
-  return null;
+  menu.appendChild(startBtn);
+  mainContent.appendChild(menu);
+
+}
+
+if (gameStatus === false) {
+  showMenu();
+} else if (gameStatus === true) {
+  mainContent.removeChildNodes();
+  mainContent.appendChild(gameIsOn);
+  console.log("gameStatus true");
 }
